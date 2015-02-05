7dc6495da9986bf94ae0376715c6f763d7357212
Bri Dotson <bridotson@gmail.com>
2015-02-05 13:41:42 -0800

    add is_active toggler to light model, add toggle_active and show methods to lights controller

--- app/controllers/lights_controller.rb
+++ app/controllers/lights_controller.rb
@@ -1,3 +1,10 @@
 class LightsController < ApplicationController
-  
+  def toggle_active
+    @light = Light.first
+    @light.toggle_active
+  end
+
+  def show
+    @light = Light.first
+  end
 end
--- app/models/light.rb
+++ app/models/light.rb
@@ -1,2 +1,5 @@
 class Light < ActiveRecord::Base
+  def toggle_active
+    update(is_active: !is_active)
+  end
 end
