d9cf74a7e41d176477dfd40a968ef307e4215eb5
Bri Dotson <bridotson@gmail.com>
2015-02-09 14:14:24 -0800

    make js for light toggle more DRY

--- app/assets/javascripts/state_settings.js
+++ app/assets/javascripts/state_settings.js
@@ -5,16 +5,13 @@ $(function() {
     $.ajax($form.attr("action"), {
       type: "POST",
       success: function(res) {
-        $(".btn-toggle-lights").toggleClass("btn-turn-on");
-        $(".btn-toggle-lights").toggleClass("btn-turn-off");
-        $(".light-status").children().first().toggleClass("lights-on");
-        $(".light-status").children().first().toggleClass("lights-off");
+        $(".btn-toggle-lights").toggleClass("btn-turn-on btn-turn-off");
+        $(".light-status").children().first().toggleClass("lights-on lights-off");
         if (res.lights_on) {
           $(".light-status").children().first().text("ON");
         } else {
           $(".light-status").children().first().text("OFF");
         }
-        console.log(res.lights_on);
       },
       error: function(jqXHR, textStatus, errorThrown) {
         alert("ERROR!!");
