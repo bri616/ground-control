36413f787025a6753642065f0ae8718b030a5656
Bri Dotson <bridotson@gmail.com>
2015-02-13 15:02:48 -0800

    refactor teleporter and log entry view, put more logic in model

--- app/controllers/log_entries_controller.rb
+++ app/controllers/log_entries_controller.rb
@@ -1,6 +1,4 @@
 class LogEntriesController < ApplicationController
   def show
-    @city = request.location.city
-    @country = request.location.country_code
   end
 end
--- app/models/teleporter.rb
+++ app/models/teleporter.rb
@@ -3,4 +3,21 @@ class Teleporter < ActiveRecord::Base
   has_one :state_setting
   has_one :mission_setting
   has_many :log_entries
+
+  def pattern_list
+    log_entries.collect { |log_entry| log_entry.light_directive}.uniq
+  end
+
+  def successful_mission_count(pattern)
+    log_entries.where("lights_on = true AND light_directive = '#{pattern}'").count
+  end
+
+  def missions_per_hour
+    log_entries.group_by_hour(:created_at).count
+  end
+
+  def successful_missions_per_hour
+    log_entries.where(lights_on: true).group_by_hour(:created_at).count
+  end
+  
 end
