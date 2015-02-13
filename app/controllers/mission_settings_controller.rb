4d073f024d8e5cc1e6e04273162cbcc54a2a3f24
Bri Dotson <bridotson@gmail.com>
2015-02-13 15:30:09 -0800

    add seattle pattern

--- app/controllers/mission_settings_controller.rb
+++ app/controllers/mission_settings_controller.rb
@@ -20,6 +20,6 @@ class MissionSettingsController < ApplicationController
   end
 
   def light_directive_list
-    @light_directive_list = ["oz", "mars", "batcave"]
+    @light_directive_list = ["oz", "mars", "batcave", "seattle"]
   end
 end
