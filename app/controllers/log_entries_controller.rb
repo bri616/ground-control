75e9bbbe91b5eeb5d78136f41911c8634342975d
Bri Dotson <bridotson@gmail.com>
2015-02-20 13:12:18 -0800

    change check to within the last second

--- app/controllers/log_entries_controller.rb
+++ app/controllers/log_entries_controller.rb
@@ -1,7 +1,7 @@
 class LogEntriesController < ApplicationController
 
   def recent_log_entries
-    count = current_teleporter.log_entries.where("created_at > ?", 2.seconds.ago).group_by_second(:created_at).count.values.sum
+    count = current_teleporter.log_entries.where("created_at > ?", 1.seconds.ago).group_by_second(:created_at).count.values.sum
     render json: { "count" => count }
   end
 
