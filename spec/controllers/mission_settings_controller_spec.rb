623ba07452b6851699d8bbe8d9d4659c9216aa35
Bri Dotson <bridotson@gmail.com>
2015-02-21 12:11:44 -0800

    sound pattern query passes rspec test that it returns expected array

--- spec/controllers/mission_settings_controller_spec.rb
+++ spec/controllers/mission_settings_controller_spec.rb
@@ -17,9 +17,16 @@ RSpec.describe MissionSettingsController, :type => :controller do
       expect(response.status).to eq 200
     end
 
-    it 'returns an array of strings in the json' do
+    it 'returns an array in the json' do
       get :current_sound_pattern, :uid => '826dev'
       expect(JSON.parse(response.body)["sound_pattern"].class).to eq Array
+    end
+
+    it 'returns the correct array of sound patterns' do
+      uid = "826dev"
+      sound_pattern_array = Teleporter.find_by(uid: uid).mission_setting.sound_pattern.sounds.collect(&:file_name)
+      get :current_sound_pattern, :uid => uid
+      expect(JSON.parse(response.body)["sound_pattern"]).to eq sound_pattern_array
     end
   end
 
